a44be13fd5e19008b648e183330dced9e6c90659
Devin Jacobs <dbuttonz@gmail.com>
2018-07-07 23:34:25 -0600

    Hello World using raylib

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
-import Lib (helloWorld)
+import Control.Concurrent (threadDelay)
+import Lib (initWindow)
 
 main :: IO ()
-main = helloWorld
+main = do
+  initWindow 200 200 "Hello World"
+  threadDelay (4 * 1000 * 1000)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Lib where
+import Foreign.C.Types (CInt(CInt))
+import Foreign.C.String (CString, newCString)
 
-helloWorld :: IO ()
-helloWorld = putStrLn "hello world"
+foreign import ccall unsafe "raylib.h InitWindow"
+    c_initWindow :: CInt -> CInt -> CString -> IO ()
+
+initWindow :: Int -> Int -> String -> IO ()
+initWindow width height title = c_initWindow (fromIntegral width) (fromIntegral height) =<< newCString title
