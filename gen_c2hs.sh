6bb21559f56d9f23952d31e7293635743bee0439
Devin Jacobs <dbuttonz@gmail.com>
2018-08-06 22:03:05 -0600

    gen_c2hs.sh

---
+++ gen_c2hs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script is for generating the intermediate c2hs files. This is helpful for debugging.
+# This script is not part of the normal build process. It is for debugging purposes only.
+
+cd src
+c2hs Internal/Types.chs -C "-I../raylib/src"
+c2hs Internal/Core.chs -C "-I../raylib/src"
